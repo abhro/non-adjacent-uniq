239ecc45cb990fd3e31decccd3a2b7b8fc3fb685
abhro <5664668+abhro@users.noreply.github.com>
2025-08-24 10:51:30 -0400

    Make option for case-insensitive version of non-adjacent-uniq.pl

--- non-adjacent-uniq.pl
+++ non-adjacent-uniq.pl
@@ -2,6 +2,9 @@
 
 # from https://stackoverflow.com/a/746695/3174398
 
+use Hash::Case::Preserve;
+##tie my(%seen), 'Hash::Case::Preserve';
+
 while (<>) {
   print $_ if (!$seen{$_} or $_ eq "\n");
   $seen{$_} = 1;
